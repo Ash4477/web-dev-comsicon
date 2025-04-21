586a182b6184969cf7293f3bd3f8d0dbaa02f25b
Abdur Rehman <107047867+arrehman3@users.noreply.github.com>
2025-04-21 13:58:34 +0500

    App file

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,26 @@
-import "./App.css";
-import Dashboard from "./Dashboard";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
+import Performance from "./pages/Performance";
+import ProjectDetails from "./pages/ProjectDetails";
+import Home from "./pages/Home";
+import CreateProject from "./pages/CreateProject";
+import JoinProject from "./pages/JoinProject";
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/performance" element={<Performance />} />
+        <Route path="/projectDetails" element={<ProjectDetails />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/create-project" element={<CreateProject/>}/>
+        <Route path="/join-project" element={<JoinProject/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
